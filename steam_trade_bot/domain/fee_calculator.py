740db9ed77c825241f4b03b6251c8a11ca11690f
Viktor Kozlov <soniclev@mail.ru>
2023-01-28 02:40:50 +0300

    Implement functions to compute market fee in both sides

--- steam_trade_bot/domain/fee_calculator.py
+++ steam_trade_bot/domain/fee_calculator.py
@@ -1,13 +1,34 @@
+import math
 from typing import NamedTuple
 
-ComputedFee = NamedTuple("ComputedFee", total=float, payload=float, game=float, steam=float)
+ComputedFee = NamedTuple(
+    "ComputedFee",
+    [("total", float), ("payload", float), ("game", float), ("steam", float)]
+)
+
+WALLET_FEE_MINIMUM = 0.01
+WALLET_FEE_PERCENT = 0.05
+WALLET_FEE_BASE = 0
 
 
 def compute_fee_from_payload(payload: float, game: float | None = None) -> ComputedFee:
-    pass
+    steam_fee = math.floor(max(payload * WALLET_FEE_PERCENT, WALLET_FEE_MINIMUM) * 100) / 100
+    game_fee = math.floor(max(payload * (game or 0.1), WALLET_FEE_MINIMUM) * 100) / 100
+    total_fee = WALLET_FEE_BASE + game_fee + steam_fee
+    total = round(payload + total_fee, 2)
+    return ComputedFee(total=total, payload=payload, game=game_fee, steam=steam_fee)
 
 
-def compute_fee_from_total(total, game: float | None = None) -> ComputedFee:
-    pass
+def compute_fee_from_total(total: float, game: float | None = None) -> ComputedFee:
+    start_payload = round(total * 0.9, 2)
+    for _ in range(100):
+        fee = compute_fee_from_payload(start_payload, game)
+        diff = total - fee.total
+        if fee.total == total:
+            return fee
+        elif fee.total > total:
+            start_payload = round(start_payload - max(total * 0.01, 0.01), 2)
+        else:
+            start_payload = round(start_payload + max(diff * 0.4, 0.01), 2)
 
-
+    raise ValueError("Failed to compute fee in 100 cycles")
---
+++ tests/unit/domain/test_fee_calculator.py
@@ -0,0 +1,78 @@
+import pytest
+
+from steam_trade_bot.domain.fee_calculator import compute_fee_from_payload, compute_fee_from_total
+
+
+@pytest.mark.parametrize(
+    "received, total",
+    [
+        (0.01, 0.03),
+        (0.09, 0.11),
+        (0.18, 0.2),
+        (0.19, 0.21),
+        (0.2, 0.23),
+        (0.59, 0.66),
+        (0.6, 0.69),
+        (1.3, 1.49),
+        (2, 2.3),
+        (3, 3.45),
+        (4, 4.60),
+        (5, 5.75),
+        (12.43, 14.29),
+        (129.43, 148.84),
+    ],
+)
+def test_csgo(received, total):
+    fee = compute_fee_from_payload(received)
+    backward_fee = compute_fee_from_total(total)
+
+    assert fee.total == total
+    assert backward_fee.payload == received
+
+
+@pytest.mark.parametrize(
+    "received, game_fee, total",
+    [
+        (0.48, 0.07, 0.57),
+        (0.56, 0.08, 0.66),
+        (0.92, 0.13, 1.09),
+    ],
+)
+def test_app_321360(received, game_fee, total):
+    fee = compute_fee_from_payload(received, game=0.15)
+    backward_fee = compute_fee_from_total(total, game=0.15)
+
+    assert fee.total == total
+    assert fee.game == game_fee
+    assert backward_fee.payload == received
+    assert backward_fee.game == game_fee
+
+
+@pytest.mark.parametrize(
+    "game_fee",
+    [
+        None,
+        0.10,
+        0.15,
+    ],
+)
+def test_fee_from_1_cent_to_2000_bucks(game_fee):
+    # test all prices from $0.01 to $2000
+    prev_price = None
+    for i in range(1, 2000_01):
+        price = round(i / 100, 2)
+        price_with_fee = compute_fee_from_payload(price, game=game_fee)
+        backward_fee = compute_fee_from_total(price_with_fee.total, game=game_fee)
+        assert backward_fee.game == game_fee or 0.01
+        assert backward_fee.payload == price
+        # need to check corner cases, like:
+        # 0.19 -> 0.21
+        # 0.19 -> 0.22
+        if prev_price:
+            while price - 0.01 > prev_price:
+                prev_price = round(prev_price + 0.01, 2)
+                price_with_fee = compute_fee_from_payload(prev_price, game=game_fee)
+                backward_fee = compute_fee_from_total(price_with_fee.total, game=game_fee)
+                assert backward_fee.game == game_fee or 0.01
+                assert backward_fee.payload == price
+        prev_price = price
